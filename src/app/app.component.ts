a6297d3d0d08c33bbefa9bc3b63f626af839c254
kotov <kotov_dl@nartis.ru>
2026-04-28 18:00:01 +0300

    feat: fromEventPattern method (rxjs) usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,40 @@
 import { Component, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from "@angular/forms";
-import { Observable, Subscription, fromEvent } from "rxjs";
-import {JsonPipe} from "@angular/common";
+import { FormsModule } from '@angular/forms';
+import { Observable, Subscription, fromEvent, fromEventPattern } from 'rxjs';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, FormsModule, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnDestroy {
   subscription: Subscription | null = null;
 
   // Создаём поток событий движения мыши
-  stream$: Observable<MouseEvent> = fromEvent<MouseEvent>(document, 'mousemove');
+  stream$: Observable<MouseEvent> = fromEvent<MouseEvent>(
+    document,
+    'mousemove'
+  );
+  mouseMoveStream$: Observable<any> = fromEventPattern(
+    (handler) => document.addEventListener('mousemove', handler),
+    (handler) => {
+      document.removeEventListener('mousemove', handler);
+      console.log('"removeEventListener" have been called');
+    }
+  );
 
   // Объект для хранения координат мыши
   coordinates: { x: number; y: number } | null = null;
 
   public startStream(): void {
-    if (!this.subscription) { // Предотвращаем множественные подписки
-      this.subscription = this.stream$.subscribe({
+    if (!this.subscription) {
+      // Предотвращаем множественные подписки
+      // this.subscription = this.stream$.subscribe({
+      this.subscription = this.mouseMoveStream$.subscribe({
         next: (event) => {
           this.coordinates = { x: event.clientX, y: event.clientY };
           console.log(`Координаты мыши: (${event.clientX}, ${event.clientY})`);
@@ -31,7 +43,7 @@ export class AppComponent implements OnDestroy {
         complete: () => {
           console.log('Поток завершен');
           this.subscription = null; // Сбрасываем подписку после завершения
-        }
+        },
       });
       console.log('Поток запущен');
     }
